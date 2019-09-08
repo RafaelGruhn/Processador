9c75373f600375ef4baa1fe628e469fcc26e6110
rafael.gruhn <rafa.gruhn@gmail.com>
2019-09-08 14:07:26 -0300

    Adiciona pipeline

--- models.py
+++ models.py
@@ -9,20 +9,8 @@ class Memory:
     data = {}
 
     def __init__(self):
-        file = open('commands.txt', 'r')
-        try:
-            for line in file:
-                line = line.replace(',', '')
-                line = line.replace('\n', '')
-                instruction = line.split(' ')
-                self.instructions.append({
-                    'command': instruction[0],
-                    'var1': instruction[1],
-                    'var2': instruction[2]
-                })
-        except Exception as e:
-            print(e)
-        file.close()
+        for instruction in range(self.cont_instructions()):
+            self.instructions.append({})
 
         file_memory = open('memory.txt', 'w')
         for i in range(20):
@@ -30,17 +18,32 @@ class Memory:
         file_memory.writelines(self.get_data())
         file_memory.close()
 
-    def output(self):
-        """Screeam all informations from data in memory.txt"""
-        file_memory = open('memory.txt', 'w')
-        file_memory.writelines(self.get_data())
-        file_memory.close()
+    def cont_instructions(self):
+        file = open('commands.txt', 'r')
+        lines = file.readlines()
+        return len(lines)
 
     def get_data(self):
         str_data = []
         for key, value in self.data.items():
             str_data.append(key + " = " + str(value) + "\n")
         return str_data
+
+    def decode(self, li=0):
+        file = open('commands.txt', 'r')
+        try:
+            lines = file.readlines()
+            line = lines[li].replace(',', '')
+            line = line.replace('\n', '')
+            instruction = line.split(' ')
+            self.instructions[li] = {
+                'command': instruction[0],
+                'var1': instruction[1] if len(instruction) > 1 else None,
+                'var2': instruction[2] if len(instruction) > 2 else None
+            }
+        except Exception as e:
+            print(e)
+        file.close()
 
 
 class Register:
@@ -51,14 +54,8 @@ class Register:
         file_registers = open('registers.txt', 'w')
         for i in range(8):
             self.registers['r' + str(i)] = ''
-        self.special_registers['SREG'] = ''
         self.special_registers['SREG1'] = ''
-        file_registers.writelines(self.get_registers())
-        file_registers.close()
-    
-    def output(self):
-        """Screeam all informations from registers in memory.txt"""
-        file_registers = open('registers.txt', 'w')
+        self.special_registers['SREG2'] = ''
         file_registers.writelines(self.get_registers())
         file_registers.close()
 
@@ -70,6 +67,23 @@ class Register:
             str_register.append(key + " = " + str(value) + "\n")
         return str_register
 
+
+class Write:
+    """Write in output."""
+    def get_class(self, register, memory):
+        self.register = register
+        self.memory = memory
+
+    def output(self):
+        """Screeam all informations from registers in memory.txt, from data in memory.txt."""
+        file_registers = open('registers.txt', 'w')
+        file_registers.writelines(self.register.get_registers())
+        file_registers.close()
+        file_memory = open('memory.txt', 'w')
+        file_memory.writelines(self.memory.get_data())
+        file_memory.close()
+
+
 class ULA:
     """ULA."""
     memory = Memory()
@@ -79,45 +93,93 @@ class ULA:
     exception = Exception("Error: Invalid Command in line %s." % li)
 
     def execute(self):
-        for self.li in range(len(self.memory.instructions)):
+        while self.li < self.memory.cont_instructions():
+            print('Number of LI: ', self.li)
+            print('Number of CI: ', self.ci)
+            self.memory.decode(self.li)
             result = None
-            if self.memory.instructions[self.li]['command'] == 'ADD':
+            if self.memory.instructions[self.li].get('command', '') == 'ADD':
                 result = self.ADD()
-            elif self.memory.instructions[self.li]['command'] == 'SUB':
+            elif self.memory.instructions[self.li].get('command', '') == 'SUB':
                 result = self.SUB()
-            elif self.memory.instructions[self.li]['command'] == 'MOV':
-                pass
-            elif self.memory.instructions[self.li]['command'] == 'GOTO':
-                pass
-            elif self.memory.instructions[self.li]['command'] == 'JBE':
-                pass
-            elif self.memory.instructions[self.li]['command'] == 'JLE':
-                pass
+            elif self.memory.instructions[self.li].get('command', '') == 'MOV':
+                self.MOV()
+            elif self.memory.instructions[self.li].get('command', '') == 'GOTO':
+                self.GOTO()
+                continue
+            elif self.memory.instructions[self.li].get('command', '') == 'SBRC':
+                if self.SBRC():
+                    self.li = self.li + 1
+                    self.set_ci()
+            elif self.memory.instructions[self.li].get('command', '') == 'SBRS':
+                if self.SBRS():
+                    self.li = self.li + 1
+                    self.set_ci()
+            elif self.memory.instructions[self.li].get('command', '') == 'HALF':
+                self.li = len(self.memory.instructions) - 1
+                self.set_ci()
             else:
                 print("Error: Invalid Command in line %s." % self.li)
                 break
             if isinstance(result, int):
                 if result == 0:
-                    self.register.special_registers['SREG'] = '1'
-                    self.register.special_registers['SREG1'] = '0'
-                elif result < 0:
-                    self.register.special_registers['SREG'] = '0'
                     self.register.special_registers['SREG1'] = '1'
-                else:
-                    self.register.special_registers['SREG'] = '0'
+                    self.register.special_registers['SREG2'] = '0'
+                elif result < 0:
                     self.register.special_registers['SREG1'] = '0'
+                    self.register.special_registers['SREG2'] = '1'
+                else:
+                    self.register.special_registers['SREG1'] = '0'
+                    self.register.special_registers['SREG2'] = '0'
 
-        self.memory.output()
-        self.register.output()
+            teste = Write()
+            teste.get_class(self.register, self.memory)
+            teste.output()
+            self.li = self.ci
+            self.set_ci()
 
     def set_ci(self, value = 1):
         self.ci = self.li + value
+
+    def SBRS(self):
+        if re.match(r'r', self.memory.instructions[self.li]['var1']):
+            return self.register.registers.get(self.memory.instructions[self.li]['var1'], 0) == 1
+        else:
+            raise self.exception
+
+    def SBRC(self):
+        if re.match(r'r', self.memory.instructions[self.li]['var1']):
+            return self.register.registers.get(self.memory.instructions[self.li]['var1'], 0) == 0
+        else:
+            raise self.exception
+
+    def GOTO(self):
+        self.li = int(self.memory.instructions[self.li].get('var1', 0))
+        self.set_ci()
 
     def ADD(self):
         return self.action(operator.add)
 
     def SUB(self):
         return self.action(operator.sub)
+
+    def MOV(self):
+        if re.match(r'&', self.memory.instructions[self.li]['var1']):
+            if re.match(r'&', self.memory.instructions[self.li]['var2']):
+                self.memory.data[self.memory.instructions[self.li]['var1']] = self.memory.data.get(self.memory.instructions[self.li]['var2'], 0)
+            elif re.match(r'r', self.memory.instructions[self.li]['var2']):
+                self.memory.data[self.memory.instructions[self.li]['var1']] = self.register.registers.get(self.memory.instructions[self.li]['var2'], 0)
+            else:
+                raise self.exception
+        elif re.match(r'r', self.memory.instructions[self.li]['var1']):
+            if re.match(r'&', self.memory.instructions[self.li]['var2']):
+                self.register.registers[self.memory.instructions[self.li]['var1']] = self.memory.data.get(self.memory.instructions[self.li]['var2'], 0)
+            elif re.match(r'r', self.memory.instructions[self.li]['var2']):
+                self.register.registers[self.memory.instructions[self.li]['var1']] = self.register.registers.get(self.memory.instructions[self.li]['var2'], 0)
+            else:
+                raise self.exception
+        else:
+            raise self.exception
 
     def action(self, operator):
         if re.match(r'&', self.memory.instructions[self.li]['var1']):
